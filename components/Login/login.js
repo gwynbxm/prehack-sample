281e033b9a7cc348700bf6cae1905668eb3b0204
Casuarina07 <casuarina070798@gmail.com>
2021-06-09 15:42:29 +0800

    Update naming cconventions

--- components/Login/login.js
+++ components/Login/login.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function login() {
+export default function Login() {
   return (
     <View style={styles.container}>
       <Text>Login Page</Text>
--- components/Profile/profile.js
+++ components/Profile/profile.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function profile() {
+export default function Profile() {
   return (
     <View style={styles.container}>
       <Text>Profile Page</Text>
