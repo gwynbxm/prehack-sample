8a42d62f16dcb2fec2191a3b238fb4d2da33e120
Gwyn Bong <gwynbong@gmail.com>
2021-06-13 16:52:09 +0800

    add profile photo from phone library

--- components/Profile/profile.js
+++ components/Profile/profile.js
@@ -1,19 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import {
-  Alert,
   ImageBackground,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
-  Image,
 } from "react-native";
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import firebase from "../../config.js";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Profile() {
   const [userData, setUserData] = useState({
@@ -24,6 +22,7 @@ export default function Profile() {
   });
 
   const [errorText, setErrorText] = useState("");
+  const [image, setImage] = useState(null);
 
   const handleChangeText = (name, value) => {
     setUserData({ ...userData, [name]: value });
@@ -46,15 +45,54 @@ export default function Profile() {
     alert("deleted user");
   };
 
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      // uploadImage(result.uri)
+      //   .then(() => {
+      //     alert("image success");
+      //   })
+      //   .catch((error) => {
+      //     alert(error);
+      //   });
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity onPress={pickImage}>
         <View style={styles.dpContainer}>
           <ImageBackground
             style={styles.profileDp}
             imageStyle={{ borderRadius: 100 }}
             source={{
-              uri: "https://lh3.googleusercontent.com/znytw2BkzEYJv20Xp-0f8TMaopbemaVWLiRirBZ217Hxuo5vgX-KAKZDLdY3VojsYWXvfm5ollyUiAvecuwdNqzJ",
+              uri: image
+                ? image
+                : userData
+                ? userData.profileUrl ||
+                  "https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg"
+                : "https://lh3.googleusercontent.com/znytw2BkzEYJv20Xp-0f8TMaopbemaVWLiRirBZ217Hxuo5vgX-KAKZDLdY3VojsYWXvfm5ollyUiAvecuwdNqzJ",
             }}
           >
             <View style={styles.cameraContainer}>
